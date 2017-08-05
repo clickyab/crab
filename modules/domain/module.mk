e91f416d4b00aa0e6f4fda51d786f1bf9f0afb2d
fzerorubigd <fzerorubigd@gmail.com>
2017-08-05 22:20:56 +0430

    add jenkins file

--- modules/domain/module.mk
+++ modules/domain/module.mk
@@ -1,4 +1,4 @@
-export DOMAIN_ROOT=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+DOMAIN_ROOT:=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 $(DOMAIN_ROOT)-codegen: tools-codegen
 	$(BIN)/codegen -p clickyab.com/crab/modules/domain/dmn
@@ -6,6 +6,9 @@ $(DOMAIN_ROOT)-codegen: tools-codegen
 
 $(DOMAIN_ROOT)-migration: tools-go-bindata
 	cd $(DOMAIN_ROOT)/migrations && $(BIN)/go-bindata -nometadata -o $(DOMAIN_ROOT)/migrations/migration.gen.go -nomemcopy=true -pkg=migrations ./db/...
+
+$(DOMAIN_ROOT)-test:
+	cd $(DOMAIN_ROOT)/ && $(GO) test ./...
 
 .PHONY: $(DOMAIN_ROOT)-codegen $(DOMAIN_ROOT)-migration
 
