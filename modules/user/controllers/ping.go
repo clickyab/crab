8eee28d35284bb56ffa8b0294cfb60856a1da622
slifer <dara51php@gmail.com>
2017-08-13 11:58:50 +0430

    fix register

--- modules/user/controllers/ping.go
+++ modules/user/controllers/ping.go
@@ -16,5 +16,5 @@ import (
 // }
 func (c Controller) ping(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	user := authz.MustGetUser(ctx)
-	c.responseLoginOKStatus(w, user.ID, user.Email, authz.MustGetToken(ctx), user.UserType)
+	c.createLoginResponse(w, user, authz.MustGetToken(ctx))
 }
