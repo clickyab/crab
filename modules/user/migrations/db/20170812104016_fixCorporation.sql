7af4a624f80b5f89e5cf8819396fbb1e0ac71ffa
slifer <dara51php@gmail.com>
2017-08-16 13:45:17 +0430

    fix migration

--- modules/user/migrations/db/20170812104016_fixCorporation.sql
+++
@@ -1,9 +0,0 @@
-
--- +migrate Up
--- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE user_corporation DROP id;
-ALTER TABLE user_corporation ADD PRIMARY KEY (user_id);
-
--- +migrate Down
--- SQL section 'Down' is executed when this migration is rolled back
-;
