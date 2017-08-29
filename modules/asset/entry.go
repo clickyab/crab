6610c23db89d720d2f0cace20a6d9c280589eeac
slifer <dara51php@gmail.com>
2017-08-29 15:34:46 +0430

    add os migration and struct
    Fix #3338

---
+++ modules/asset/entry.go
@@ -0,0 +1,7 @@
+package asset
+
+import (
+	_ "clickyab.com/crab/modules/asset/ast"
+	_ "clickyab.com/crab/modules/asset/controllers"
+
+)
---
+++ modules/asset/module.mk
@@ -0,0 +1,13 @@
+export ASSET_ROOT:=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+
+$(ASSET_ROOT)-codegen: tools-codegen
+	$(BIN)/codegen -p clickyab.com/crab/modules/asset/ast
+	$(BIN)/codegen -p clickyab.com/crab/modules/asset/controllers
+
+$(ASSET_ROOT)-migration: tools-go-bindata
+	cd $(ASSET_ROOT)/migrations && $(BIN)/go-bindata -nometadata -o $(ASSET_ROOT)/migrations/migration.gen.go -nomemcopy=true -pkg=migrations ./db/...
+
+$(ASSET_ROOT)-test:
+	cd $(ASSET_ROOT)/ && $(GO) test ./...
+
+.PHONY: $(ASSET_ROOT)-codegen $(ASSET_ROOT)-migration
