c6c15d64b62e96501a64af78f5c38daf1d3ec6a6
Kian Ostad <c@kianostad.com>
2017-09-02 19:51:06 +0430

    Add inventory routes
    fix #3341 #3340

--- modules/inventory/controllers/inventory.go
+++ modules/inventory/controllers/inventory.go
@@ -1,9 +1,69 @@
 package controllers
 
-import "github.com/clickyab/services/framework/controller"
+import (
+	"context"
+	"net/http"
 
+	"errors"
+	"fmt"
+	"strconv"
+
+	"clickyab.com/crab/modules/inventory/orm"
+	"clickyab.com/crab/modules/user/middleware/authz"
+	"github.com/clickyab/services/assert"
+	"github.com/clickyab/services/framework/controller"
+	"github.com/clickyab/services/trans"
+	"github.com/rs/xmux"
+)
+
+// Controller is the controller for the location package
+// @Route {
+// 		middleware = domain.Access
+//		group = /inventory
+// }
 type Controller struct {
 	controller.Base
 }
 
+type inventories []orm.Inventory
+
+// Inventories return all user inventories
+// @Route {
+// 		url = /:user
+//		method = get
+//		200 = inventories
+//		middleware = authz.Authenticate
+// }
+func (ctrl *Controller) Inventories(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	u := authz.MustGetUser(ctx)
+	res, e := orm.NewOrmManager().FindInventoryByUserID(u.ID)
+	assert.Nil(e)
+	if res == nil {
+		ctrl.NotFoundResponse(w, trans.E("User doesn't have any list"))
+		return
+	}
+	ctrl.OKResponse(w, res)
+}
+
+// Inventory return a user inventory
+// @Route {
+// 		url = /:id
+//		method = get
+//		200 = orm.Inventory
+//		middleware = authz.Authenticate
+// }
+func (ctrl *Controller) Inventory(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	id, e := strconv.ParseInt(xmux.Param(ctx, "id"), 10, 64)
+	if e != nil {
+		ctrl.BadResponse(w, errors.New(fmt.Sprintf("not valid id")))
+		return
+	}
+	res, e := orm.NewOrmManager().FindInventoryByID(id)
+	assert.Nil(e)
+	if res == nil {
+		ctrl.NotFoundResponse(w, trans.E("Inventory with id %s does not exists!", id))
+		return
+	}
+	ctrl.OKResponse(w, res)
+}
 
--- modules/inventory/module.mk
+++ modules/inventory/module.mk
@@ -1,8 +1,8 @@
 export INVENTORY_ROOT:=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 $(INVENTORY_ROOT)-codegen: tools-codegen
-	$(BIN)/codegen -p clickyab.com/crab/modules/upload/orm
-	$(BIN)/codegen -p clickyab.com/crab/modules/upload/controllers
+	$(BIN)/codegen -p clickyab.com/crab/modules/inventory/orm
+	$(BIN)/codegen -p clickyab.com/crab/modules/inventory/controllers
 
 $(INVENTORY_ROOT)-migration: tools-go-bindata
 	cd $(INVENTORY_ROOT)/migrations && $(BIN)/go-bindata -nometadata -o $(INVENTORY_ROOT)/migrations/migration.gen.go -nomemcopy=true -pkg=migrations ./db/...
