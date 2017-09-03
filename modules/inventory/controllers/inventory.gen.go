68625f795a5126265b209dfcfac5c9e5ceb2fc33
Kian Ostad <c@kianostad.com>
2017-09-03 14:20:41 +0430

    Create inventory
    fix #3393

--- modules/inventory/controllers/inventory.gen.go
+++ modules/inventory/controllers/inventory.gen.go
@@ -8,6 +8,7 @@ import (
 	"clickyab.com/crab/modules/domain/middleware/domain"
 	"clickyab.com/crab/modules/user/middleware/authz"
 	"github.com/clickyab/services/framework"
+	"github.com/clickyab/services/framework/middleware"
 	"github.com/clickyab/services/framework/router"
 	"github.com/clickyab/services/initializer"
 	"github.com/rs/xhandler"
@@ -27,9 +28,9 @@ func (ctrl *Controller) Routes(r *xmux.Mux, mountPoint string) {
 		group := r.NewGroup(mountPoint + "/inventory")
 
 		/* Route {
-			"Route": "/:user",
+			"Route": "/presets",
 			"Method": "GET",
-			"Function": "Controller.Inventories",
+			"Function": "Controller.whiteBlackLists",
 			"RoutePkg": "controllers",
 			"RouteMiddleware": [
 				"authz.Authenticate"
@@ -45,13 +46,13 @@ func (ctrl *Controller) Routes(r *xmux.Mux, mountPoint string) {
 			authz.Authenticate,
 		}...)
 
-		group.GET("/:user", xhandler.HandlerFuncC(framework.Mix(ctrl.Inventories, m0...)))
+		group.GET("/presets", xhandler.HandlerFuncC(framework.Mix(ctrl.whiteBlackLists, m0...)))
 		// End route with key 0
 
 		/* Route {
-			"Route": "/:id",
+			"Route": "/preset/:id",
 			"Method": "GET",
-			"Function": "Controller.Inventory",
+			"Function": "Controller.whiteBlackList",
 			"RoutePkg": "controllers",
 			"RouteMiddleware": [
 				"authz.Authenticate"
@@ -67,8 +68,32 @@ func (ctrl *Controller) Routes(r *xmux.Mux, mountPoint string) {
 			authz.Authenticate,
 		}...)
 
-		group.GET("/:id", xhandler.HandlerFuncC(framework.Mix(ctrl.Inventory, m1...)))
+		group.GET("/preset/:id", xhandler.HandlerFuncC(framework.Mix(ctrl.whiteBlackList, m1...)))
 		// End route with key 1
+
+		/* Route {
+			"Route": "/preset",
+			"Method": "POST",
+			"Function": "Controller.addPreset",
+			"RoutePkg": "controllers",
+			"RouteMiddleware": [
+				"authz.Authenticate"
+			],
+			"RouteFuncMiddleware": "",
+			"RecType": "Controller",
+			"RecName": "ctrl",
+			"Payload": "whiteBlackList",
+			"Resource": "",
+			"Scope": ""
+		} with key 2 */
+		m2 := append(groupMiddleware, []framework.Middleware{
+			authz.Authenticate,
+		}...)
+
+		// Make sure payload is the last middleware
+		m2 = append(m2, middleware.PayloadUnMarshallerGenerator(whiteBlackList{}))
+		group.POST("/preset", xhandler.HandlerFuncC(framework.Mix(ctrl.addPreset, m2...)))
+		// End route with key 2
 
 		initializer.DoInitialize(ctrl)
 	})
--- modules/inventory/orm/inventory.gen.go
+++ modules/inventory/orm/inventory.gen.go
@@ -13,44 +13,44 @@ import (
 
 // AUTO GENERATED CODE. DO NOT EDIT!
 
-// CreateInventory try to save a new Inventory in database
-func (m *Manager) CreateInventory(i *Inventory) error {
+// CreateWhiteBlackList try to save a new WhiteBlackList in database
+func (m *Manager) CreateWhiteBlackList(wbl *WhiteBlackList) error {
 	now := time.Now()
-	i.CreatedAt = now
-	i.UpdatedAt = now
+	wbl.CreatedAt = now
+	wbl.UpdatedAt = now
 	func(in interface{}) {
 		if ii, ok := in.(initializer.Simple); ok {
 			ii.Initialize()
 		}
-	}(i)
+	}(wbl)
 
-	return m.GetWDbMap().Insert(i)
+	return m.GetWDbMap().Insert(wbl)
 }
 
-// UpdateInventory try to update Inventory in database
-func (m *Manager) UpdateInventory(i *Inventory) error {
+// UpdateWhiteBlackList try to update WhiteBlackList in database
+func (m *Manager) UpdateWhiteBlackList(wbl *WhiteBlackList) error {
 	now := time.Now()
-	i.UpdatedAt = now
+	wbl.UpdatedAt = now
 	func(in interface{}) {
 		if ii, ok := in.(initializer.Simple); ok {
 			ii.Initialize()
 		}
-	}(i)
+	}(wbl)
 
-	_, err := m.GetWDbMap().Update(i)
+	_, err := m.GetWDbMap().Update(wbl)
 	return err
 }
 
-// ListInventoriesWithFilter try to list all Inventories without pagination
-func (m *Manager) ListInventoriesWithFilter(filter string, params ...interface{}) []Inventory {
+// ListWhiteBlackListsWithFilter try to list all WhiteBlackLists without pagination
+func (m *Manager) ListWhiteBlackListsWithFilter(filter string, params ...interface{}) []WhiteBlackList {
 	filter = strings.Trim(filter, "\n\t ")
 	if filter != "" {
 		filter = "WHERE " + filter
 	}
-	var res []Inventory
+	var res []WhiteBlackList
 	_, err := m.GetRDbMap().Select(
 		&res,
-		fmt.Sprintf("SELECT * FROM %s %s", InventoryTableFull, filter),
+		fmt.Sprintf("SELECT * FROM %s %s", WhiteBlackListTableFull, filter),
 		params...,
 	)
 	assert.Nil(err)
@@ -58,19 +58,19 @@ func (m *Manager) ListInventoriesWithFilter(filter string, params ...interface{}
 	return res
 }
 
-// ListInventories try to list all Inventories without pagination
-func (m *Manager) ListInventories() []Inventory {
-	return m.ListInventoriesWithFilter("")
+// ListWhiteBlackLists try to list all WhiteBlackLists without pagination
+func (m *Manager) ListWhiteBlackLists() []WhiteBlackList {
+	return m.ListWhiteBlackListsWithFilter("")
 }
 
-// CountInventoriesWithFilter count entity in Inventories table with valid where filter
-func (m *Manager) CountInventoriesWithFilter(filter string, params ...interface{}) int64 {
+// CountWhiteBlackListsWithFilter count entity in WhiteBlackLists table with valid where filter
+func (m *Manager) CountWhiteBlackListsWithFilter(filter string, params ...interface{}) int64 {
 	filter = strings.Trim(filter, "\n\t ")
 	if filter != "" {
 		filter = "WHERE " + filter
 	}
 	cnt, err := m.GetRDbMap().SelectInt(
-		fmt.Sprintf("SELECT COUNT(*) FROM %s %s", InventoryTableFull, filter),
+		fmt.Sprintf("SELECT COUNT(*) FROM %s %s", WhiteBlackListTableFull, filter),
 		params...,
 	)
 	assert.Nil(err)
@@ -78,15 +78,15 @@ func (m *Manager) CountInventoriesWithFilter(filter string, params ...interface{
 	return cnt
 }
 
-// CountInventories count entity in Inventories table
-func (m *Manager) CountInventories() int64 {
-	return m.CountInventoriesWithFilter("")
+// CountWhiteBlackLists count entity in WhiteBlackLists table
+func (m *Manager) CountWhiteBlackLists() int64 {
+	return m.CountWhiteBlackListsWithFilter("")
 }
 
-// ListInventoriesWithPaginationFilter try to list all Inventories with pagination and filter
-func (m *Manager) ListInventoriesWithPaginationFilter(
-	offset, perPage int, filter string, params ...interface{}) []Inventory {
-	var res []Inventory
+// ListWhiteBlackListsWithPaginationFilter try to list all WhiteBlackLists with pagination and filter
+func (m *Manager) ListWhiteBlackListsWithPaginationFilter(
+	offset, perPage int, filter string, params ...interface{}) []WhiteBlackList {
+	var res []WhiteBlackList
 	filter = strings.Trim(filter, "\n\t ")
 	if filter != "" {
 		filter = "WHERE " + filter
@@ -98,7 +98,7 @@ func (m *Manager) ListInventoriesWithPaginationFilter(
 	// TODO : better pagination without offset and limit
 	_, err := m.GetRDbMap().Select(
 		&res,
-		fmt.Sprintf("SELECT * FROM %s %s", InventoryTableFull, filter),
+		fmt.Sprintf("SELECT * FROM %s %s", WhiteBlackListTableFull, filter),
 		params...,
 	)
 	assert.Nil(err)
@@ -106,34 +106,18 @@ func (m *Manager) ListInventoriesWithPaginationFilter(
 	return res
 }
 
-// ListInventoriesWithPagination try to list all Inventories with pagination
-func (m *Manager) ListInventoriesWithPagination(offset, perPage int) []Inventory {
-	return m.ListInventoriesWithPaginationFilter(offset, perPage, "")
+// ListWhiteBlackListsWithPagination try to list all WhiteBlackLists with pagination
+func (m *Manager) ListWhiteBlackListsWithPagination(offset, perPage int) []WhiteBlackList {
+	return m.ListWhiteBlackListsWithPaginationFilter(offset, perPage, "")
 }
 
-// FindInventoryByID return the Inventory base on its id
-func (m *Manager) FindInventoryByID(id int64) (*Inventory, error) {
-	var res Inventory
+// FindWhiteBlackListByID return the WhiteBlackList base on its id
+func (m *Manager) FindWhiteBlackListByID(id int64) (*WhiteBlackList, error) {
+	var res WhiteBlackList
 	err := m.GetRDbMap().SelectOne(
 		&res,
-		fmt.Sprintf("SELECT * FROM %s WHERE id=?", InventoryTableFull),
+		fmt.Sprintf("SELECT * FROM %s WHERE id=?", WhiteBlackListTableFull),
 		id,
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &res, nil
-}
-
-// FindInventoryByUserID return the Inventory base on its user_id
-func (m *Manager) FindInventoryByUserID(ui int64) (*Inventory, error) {
-	var res Inventory
-	err := m.GetRDbMap().SelectOne(
-		&res,
-		fmt.Sprintf("SELECT * FROM %s WHERE user_id=?", InventoryTableFull),
-		ui,
 	)
 
 	if err != nil {
