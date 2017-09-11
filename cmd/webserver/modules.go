5e18951e1c91451f3cc26e2523ab205504dd294a
daniel <danicheeta@yaoo.com>
2017-09-11 12:19:22 +0430

    inventory fix

--- cmd/webserver/modules.go
+++ cmd/webserver/modules.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	_ "clickyab.com/crab/modules/asset"
 	_ "clickyab.com/crab/modules/domain"
+	_ "clickyab.com/crab/modules/inventory"
 	_ "clickyab.com/crab/modules/location"
 	_ "clickyab.com/crab/modules/misc"
-	_ "clickyab.com/crab/modules/asset"
+	_ "clickyab.com/crab/modules/upload"
 	_ "clickyab.com/crab/modules/user"
 	_ "github.com/clickyab/services/kv/redis"
-	_ "clickyab.com/crab/modules/upload"
 )
