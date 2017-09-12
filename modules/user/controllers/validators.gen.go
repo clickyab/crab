1eec9defba689ecd1d3adec25cb1de6d12f72464
Kian Ostad <c@kianostad.com>
2017-09-12 19:02:29 +0430

    rewrite email verification

--- modules/user/controllers/validators.gen.go
+++ modules/user/controllers/validators.gen.go
@@ -13,41 +13,6 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
-func (pl *checkActivePayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	err := func(in interface{}) error {
-		if v, ok := in.(interface {
-			ValidateExtra(ctx context.Context, w http.ResponseWriter, r *http.Request) error
-		}); ok {
-			return v.ValidateExtra(ctx, w, r)
-		}
-		return nil
-	}(pl)
-	if err != nil {
-		return err
-	}
-	errs := validator.New().Struct(pl)
-	if errs == nil {
-		return nil
-	}
-	res := middleware.GroupError{}
-	for _, i := range errs.(validator.ValidationErrors) {
-		switch i.Field() {
-		case "Email":
-			res["email"] = trans.E("invalid value")
-
-		case "Number":
-			res["number"] = trans.E("invalid value")
-
-		default:
-			logrus.Panicf("the field %s is not translated", i)
-		}
-	}
-	if len(res) > 0 {
-		return res
-	}
-	return nil
-}
-
 func (pl *checkMailPayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	err := func(in interface{}) error {
 		if v, ok := in.(interface {
@@ -294,7 +259,7 @@ func (pl *registerPayload) Validate(ctx context.Context, w http.ResponseWriter, 
 	return nil
 }
 
-func (pl *sendActivePayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+func (pl *verifyResendPayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	err := func(in interface{}) error {
 		if v, ok := in.(interface {
 			ValidateExtra(ctx context.Context, w http.ResponseWriter, r *http.Request) error
@@ -314,7 +279,7 @@ func (pl *sendActivePayload) Validate(ctx context.Context, w http.ResponseWriter
 	for _, i := range errs.(validator.ValidationErrors) {
 		switch i.Field() {
 		case "Email":
-			res["email"] = trans.E("invalid value")
+			res["email_string"] = trans.E("invalid value")
 
 		default:
 			logrus.Panicf("the field %s is not translated", i)
