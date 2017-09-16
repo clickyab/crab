29e1d7c08ecddb39eb0df6ca1f11a9e97f0569f4
Kian Ostad <c@kianostad.com>
2017-09-16 21:47:22 +0430

    Add campaign routes
    fix #3632

--- modules/campaign/orm/campaign.go
+++ modules/campaign/orm/campaign.go
@@ -5,267 +5,141 @@ import (
 
 	"errors"
 
+	"database/sql"
+
 	"clickyab.com/crab/modules/domain/dmn"
-	"clickyab.com/crab/modules/inventory/orm"
 	"clickyab.com/crab/modules/user/aaa"
 	"github.com/clickyab/services/assert"
-	_ "github.com/clickyab/services/assert"
-	"github.com/clickyab/services/mysql"
-	_ "github.com/clickyab/services/mysql"
-)
-
-// UserValidStatus is the user status
-type (
-	// UserValidStatus is the user status
-	// @Enum{
-	// }
-	UserValidStatus string
 )
 
 const (
-	// RegisteredUserStatus user registered
-	RegisteredUserStatus UserValidStatus = "registered"
-	// BlockedUserStatus user blocked
-	BlockedUserStatus UserValidStatus = "blocked"
-	// ActiveUserStatus user active
-	ActiveUserStatus UserValidStatus = "active"
+	// Foreign is filter for region is every where except iran
+	Foreign = "foreign"
 )
 
-// CampaignKind is kind of campaign <web,app>
-// @Enum{
-// }
-type CampaignKind string
+// ErrInventoryID of insert or update campaign
+var ErrInventoryID = errors.New("there is no inventory with this id")
 
-const (
-	// WebCampaign is web
-	WebCampaign CampaignKind = "web"
-	// AppCampaign is app
-	AppCampaign = "app"
-)
+// AddCampaign for creating campaign with minimum info
+func (m *Manager) AddCampaign(c CampaignBase, u *aaa.User, d *dmn.Domain) (*Campaign, error) {
+	ca := &Campaign{
+		base: base{
+			Active: true,
+		},
+		DomainID: d.ID,
+		UserID:   u.ID,
+		CampaignFinance: CampaignFinance{
 
-// CampaignType is type of campaign <vast,banner,native>
-// @Enum{
-// }
-type CampaignType string
+			CostType: CPC,
+		},
+		CampaignBaseType: CampaignBaseType{
 
-const (
-	BannerType CampaignType = "banner"
-	VastType   CampaignType = "vast"
-	NativeType CampaignType = "native"
-)
+			Type: c.Type,
+			Kind: c.Kind,
+		},
+		CampaignStatus: CampaignStatus{
 
-// CostType is type of campaign <cpm,cpc,cpa>
-// @Enum{
-// }
-type CostType string
-
-const (
-	// CPM is cpm
-	CPM CostType = "cpm"
-	// CPC is cpc
-	CPC CostType = "cpc"
-	// CPA is cpa
-	CPA CostType = "cpa"
-)
-
-// Campaign campaign model in database
-// @Model {
-//		table = campaigns
-//		primary = true, id
-//		find_by = id
-//		list = yes
-// }
-type Campaign struct {
-	ID           int64           `json:"id" db:"id"`
-	CreatedAt    time.Time       `json:"created_at" db:"created_at"`
-	UpdatedAt    time.Time       `json:"updated_at" db:"updated_at"`
-	Active       bool            `json:"active" db:"active"`
-	UserID       int64           `json:"user_id" db:"user_id"`
-	DomainID     int64           `json:"domain_id" db:"domain_id"`
-	Kind         CampaignKind    `json:"kind" db:"kind"`
-	Type         CampaignType    `json:"type" db:"type"`
-	Status       bool            `json:"status" db:"status"`
-	StartAt      time.Time       `json:"start_at" db:"start_at"`
-	EndAt        time.Time       `json:"end_at" db:"end_at"`
-	Title        string          `json:"title" db:"title"`
-	Budget       int64           `json:"budget" db:"budget"`
-	Daily_limit  int64           `json:"daily_limit" db:"daily_limit"`
-	CostType     CostType        `json:"cost_type" db:"cost_type"`
-	CPCCost      int64           `json:"cpc_cost" db:"cpc_cost"`
-	WhiteBlackID mysql.NullInt64 `json:"white_black_id" db:"white_black_id"`
-	// WhiteBlackType true is whitelist
-	WhiteBlackType  mysql.NullBool        `json:"white_black_id"db:"white_black_id"`
-	WhiteBlackValue mysql.StringJSONArray `json:"white_blacklist"db:"white_blacklist"`
-}
-
-// CampaignAttributes model in database
-// @Model {
-//		table = campaign_attributes
-//		primary = true, id
-//		find_by = id, campaign_id
-//		list = yes
-// 		belong = Campaign:campaign_id
-// }
-type CampaignAttributes struct {
-	ID           int64                 `json:"id" db:"id"`
-	CampaignID   int64                 `json:"campaign_id" db:"campaign_id"`
-	Email        mysql.StringJSONArray `json:"email" db:"email"`
-	Device       mysql.StringJSONArray `json:"device" db:"device"`
-	Manufacturer mysql.StringJSONArray `json:"manufacturer" db:"manufacturer"`
-	OS           mysql.StringJSONArray `json:"os" db:"os"`
-	Browser      mysql.StringJSONArray `json:"browser" db:"browser"`
-	IAB          mysql.StringJSONArray `json:"iab" db:"iab"`
-	Region       mysql.StringJSONArray `json:"region" db:"region"`
-	Cellular     mysql.StringJSONArray `json:"cellular" db:"cellular"`
-	ISP          mysql.StringJSONArray `json:"isp" db:"isp"`
-}
-
-
-type CampaignPayload struct {
-	Kind         CampaignKind `json:"kind"`
-	Type         CampaignType `json:"type"`
-	Status       bool         `json:"status"`
-	StartAt      time.Time    `json:"start_at"`
-	EndAt        time.Time    `json:"end_at"`
-	Title        string       `json:"title"`
-	Budget       int64        `json:"budget"`
-	Daily_limit  int64        `json:"daily_limit"`
-	CostType     CostType     `json:"cost_type"`
-	CPCCost      int64        `json:"cpc_cost"`
-	WhiteBlackID int64        `json:"white_black_id"`
-	Email        []string     `json:"email"`
-	Device       []string     `json:"device"`
-	Manufacturer []string     `json:"manufacturer"`
-	OS           []string     `json:"os"`
-	Browser      []string     `json:"browser"`
-	IAB          []string     `json:"iab"`
-	Region       []string     `json:"region"`
-	Cellular     []string     `json:"cellular"`
-	ISP          []string     `json:"isp"`
-	H00          bool         `json:"h00"`
-	H01          bool         `json:"h01"`
-	H02          bool         `json:"h02"`
-	H03          bool         `json:"h03"`
-	H04          bool         `json:"h04"`
-	H05          bool         `json:"h05"`
-	H06          bool         `json:"h06"`
-	H07          bool         `json:"h07"`
-	H08          bool         `json:"h08"`
-	H09          bool         `json:"h09"`
-	H10          bool         `json:"h10"`
-	H11          bool         `json:"h11"`
-	H12          bool         `json:"h12"`
-	H13          bool         `json:"h13"`
-	H14          bool         `json:"h14"`
-	H15          bool         `json:"h15"`
-	H16          bool         `json:"h16"`
-	H17          bool         `json:"h17"`
-	H18          bool         `json:"h18"`
-	H19          bool         `json:"h19"`
-	H20          bool         `json:"h20"`
-	H21          bool         `json:"h21"`
-	H22          bool         `json:"h22"`
-	H23          bool         `json:"h23"`
-}
-
-func (m *Manager) AddCampaign(p CampaignPayload, u *aaa.User, d *dmn.Domain) (int64, error) {
-	c := &Campaign{}
-	now := time.Now()
-	c.CreatedAt = now
-	c.UpdatedAt = now
-	c.Active = true
-	c.UserID = u.ID
-	c.DomainID = d.ID
-
-	c.Kind = p.Kind
-	c.Type = p.Type
-	c.Status = p.Status
-	c.StartAt = p.StartAt
-	c.EndAt = p.EndAt
-	c.Title = p.Title
-	c.Budget = p.Budget
-	c.Daily_limit = p.Daily_limit
-	c.CostType = p.CostType
-	c.CPCCost = p.CPCCost
-
-	if p.WhiteBlackID > 1 {
-		n, e := orm.NewOrmManager().FindWhiteBlackListByID(p.WhiteBlackID)
-		if e != nil {
-			return 0, errors.New("there is no inventory with this id")
-		}
-		c.WhiteBlackID = mysql.NullInt64{
-			Valid: true,
-			Int64: p.WhiteBlackID,
-		}
-		c.WhiteBlackType = mysql.NullBool{
-			Valid: true,
-			Bool:  n.Kind,
-		}
-		c.WhiteBlackValue = n.Domains
+			Status:  c.Status,
+			StartAt: c.StartAt,
+			EndAt:   c.EndAt,
+			Title:   c.Title,
+		},
+		Progress: ProgressInProgress,
 	}
+	switch c.Kind {
+	case WebCampaign:
+		ca.webMaxBid(c)
+	case AppCampaign:
+		ca.appMaxBid(c)
+	}
+	err := m.Begin()
+	assert.Nil(err)
+	defer func() {
+		if err == nil {
+			assert.Nil(m.Commit())
+			return
+		}
+		assert.Nil(m.Rollback())
+	}()
+
+	if err = m.CreateCampaign(ca); err != nil {
+		return nil, err
+	}
+	s := &Schedule{
+		CampaignID:    ca.ID,
+		ScheduleSheet: c.Schedule,
+	}
+	ca.Schedule = s.ScheduleSheet
+	err = m.CreateSchedule(s)
+	return ca, err
+}
+
+var (
+	// ErrorStartDate should raise if campaign start date is not valid
+	ErrorStartDate = errors.New("start date can't be past")
+)
+
+// UpdateCampaignByID for updating campaign with minimum info
+func (m *Manager) UpdateCampaignByID(c CampaignStatus, ca *Campaign) error {
+
+	ca.Status = c.Status
+	if ca.StartAt != c.StartAt {
+		today, err := time.Parse("02-01-03", time.Now().Format("02-01-03"))
+		assert.Nil(err)
+		if c.StartAt.Unix() < today.Unix() {
+			return ErrorStartDate
+		}
+	}
+	ca.StartAt = c.StartAt
+	ca.EndAt = c.EndAt
+	ca.Title = c.Title
 
 	err := m.Begin()
 	assert.Nil(err)
 	defer func() {
-		if err != nil {
-			m.Rollback()
+		if err == nil {
+			assert.Nil(m.Commit())
 			return
 		}
-		m.Commit()
+		assert.Nil(m.Rollback())
 	}()
 
-	err = m.CreateCampaign(c)
+	s, err := m.FindScheduleByCampaignID(ca.ID)
 	if err != nil {
-		return 0, err
+		return err
 	}
-	a := &CampaignAttributes{}
-	a.Email = p.Email
-	a.Device = p.Device
-	a.Manufacturer = p.Manufacturer
-	a.OS = p.OS
-	a.Browser = p.Browser
-	a.IAB = p.IAB
-	a.Region = p.Region
-	a.Cellular = p.Cellular
-	a.ISP = p.ISP
-	a.CampaignID = c.ID
+	s.ScheduleSheet = c.Schedule
 
-	err = m.CreateCampaignAttributes(a)
+	err = m.UpdateCampaign(ca)
 	if err != nil {
-		return 0, err
+		return err
 	}
 
-	h := &Schedule{}
-	h.CampaignID = c.ID
-	h.UpdatedAt = now
-	h.H00 = p.H00
-	h.H01 = p.H01
-	h.H02 = p.H02
-	h.H03 = p.H03
-	h.H04 = p.H04
-	h.H05 = p.H05
-	h.H06 = p.H06
-	h.H07 = p.H07
-	h.H08 = p.H08
-	h.H09 = p.H09
-	h.H10 = p.H10
-	h.H11 = p.H11
-	h.H12 = p.H12
-	h.H13 = p.H13
-	h.H14 = p.H14
-	h.H15 = p.H15
-	h.H16 = p.H16
-	h.H17 = p.H17
-	h.H18 = p.H18
-	h.H19 = p.H19
-	h.H20 = p.H20
-	h.H21 = p.H21
-	h.H22 = p.H22
-	h.H23 = p.H23
-	err = m.CreateSchedule(h)
-	if err != nil {
-		return 0, err
-	}
-	return c.ID, nil
+	m.attachAttribute(ca)
+	err = m.UpdateSchedule(s)
+	return err
+}
 
+// Finalize will mark campaign ready for publish
+func (m *Manager) Finalize(ca *Campaign) {
+
+	ca.Progress = ProgressFinalized
+	assert.Nil(m.UpdateCampaign(ca))
+	m.attachAttribute(ca)
+	m.attachSchedule(ca)
+
+}
+
+func (m *Manager) attachSchedule(c *Campaign) {
+	s, err := m.FindScheduleByCampaignID(c.ID)
+	assert.Nil(err)
+	c.Schedule = s.ScheduleSheet
+}
+
+func (m *Manager) attachAttribute(c *Campaign) {
+	s, err := m.FindCampaignAttributesByCampaignID(c.ID)
+	if err != sql.ErrNoRows {
+		assert.Nil(err)
+	}
+	c.Attributes = s
 }
