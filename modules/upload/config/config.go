86805623a3c58d5f52edc10dac9db97ce9ac8f87
slifer <dara51php@gmail.com>
2017-09-16 16:03:31 +0430

    fix upload bug
    Ref #3567

--- modules/upload/config/config.go
+++
@@ -1,5 +0,0 @@
-package config
-
-import "github.com/clickyab/services/config"
-
-var Path = config.RegisterString("crab.modules.upload.path", "/upload", "a path to the location that uploaded file should save")
--- modules/upload/controllers/upload.go
+++ modules/upload/controllers/upload.go
@@ -11,10 +11,10 @@ import (
 	"sync"
 	"time"
 
-	"clickyab.com/crab/modules/upload/config"
 	"clickyab.com/crab/modules/upload/model"
 	"clickyab.com/crab/modules/user/middleware/authz"
 	"github.com/clickyab/services/assert"
+	"github.com/clickyab/services/config"
 	"github.com/clickyab/services/framework/controller"
 	"github.com/clickyab/services/random"
 	"github.com/rs/xmux"
@@ -27,6 +27,8 @@ type kind struct {
 	maxSize int64
 	mimes   []string
 }
+
+var uPath = config.RegisterString("crab.modules.upload.path", "/statics/uploads", "a path to the location that uploaded file should save")
 
 // Register add a route and settings for uploads
 // name will be the route, maxsize is maximum allowed size for file upload file and the mimes is alloed mime types
@@ -68,7 +70,11 @@ func (c Controller) Upload(ctx context.Context, w http.ResponseWriter, r *http.R
 		c.NotFoundResponse(w, fmt.Errorf("Not found"))
 		return
 	}
-	r.ParseMultipartForm(s.maxSize)
+	err := r.ParseMultipartForm(s.maxSize)
+	if err != nil {
+		c.BadResponse(w, fmt.Errorf("max upload size is : %d", s.maxSize))
+		return
+	}
 	file, handler, err := r.FormFile("file")
 	if err != nil {
 		c.BadResponse(w, fmt.Errorf(`file not found, the key for file should be "file"`))
@@ -84,19 +90,19 @@ func (c Controller) Upload(ctx context.Context, w http.ResponseWriter, r *http.R
 	ext := filepath.Ext(handler.Filename)
 
 	now := time.Now()
-	fp := fmt.Sprintf("%s/%s/", m, now.Format("2006/01/02"))
-	os.MkdirAll(fp, os.ModeDir)
+	fp := filepath.Join(uPath.String(), now.Format("2006/01/02"))
 
+	err = os.MkdirAll(fp, os.ModeDir|os.ModePerm)
+	assert.Nil(err)
 	fn := func() string {
 		for {
-			tmp := fmt.Sprintf("%s_%s.%s", u.ID, <-random.ID, ext)
+			tmp := fmt.Sprintf("%d_%s%s", u.ID, <-random.ID, ext)
 			if _, err := os.Stat(fp + tmp); os.IsNotExist(err) {
 				return tmp
 			}
 		}
 	}()
-
-	f, err := os.Create(fmt.Sprintf("%s/%s", config.Path, fn))
+	f, err := os.Create(fmt.Sprintf("%s/%s", uPath.String(), fn))
 	assert.Nil(err)
 	defer f.Close()
 
@@ -104,7 +110,7 @@ func (c Controller) Upload(ctx context.Context, w http.ResponseWriter, r *http.R
 	assert.Nil(er)
 
 	e := model.NewModelManager().CreateUpload(&model.Upload{
-		Path:    fn,
+		Path:    filepath.Join(now.Format("2006/01/02"), fn),
 		MIME:    mime,
 		Size:    size,
 		UserID:  u.ID,
@@ -112,7 +118,11 @@ func (c Controller) Upload(ctx context.Context, w http.ResponseWriter, r *http.R
 	})
 	assert.Nil(e)
 
-	c.JSON(w, http.StatusOK, fn)
+	c.JSON(w, http.StatusOK, struct {
+		Src string `json:"src"`
+	}{
+		Src: filepath.Join(now.Format("2006/01/02"), fn),
+	})
 }
 
 func validMIME(a textproto.MIMEHeader, b []string) (bool, string) {
