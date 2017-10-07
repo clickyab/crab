9e68fe954f67bd529dc112fa352a4a1b8743fc38
fzerorubigd <fzerorubigd@gmail.com>
2017-10-07 09:57:22 +0330

    add cors middleware

--- modules/misc/entry.go
+++ modules/misc/entry.go
@@ -1,5 +1,6 @@
 package user
 
 import (
-	_ "clickyab.com/crab/modules/misc/controllers" // import init
+	_ "clickyab.com/crab/modules/misc/controllers"     // import init
+	_ "clickyab.com/crab/modules/misc/middleware/cors" // cors global middleware
 )
