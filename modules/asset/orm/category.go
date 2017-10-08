34a182432409f2d81092790b239a2cb011ad4042
Kian Ostad <c@kianostad.com>
2017-10-08 13:15:00 +0330

    Fix category bug
    fix #3822

--- modules/asset/orm/category.go
+++ modules/asset/orm/category.go
@@ -11,5 +11,5 @@ type Category struct {
 	ID          int64  `json:"id" db:"id"`
 	Name        string `json:"name" db:"name"`
 	Description string `json:"description" db:"description"`
-	Status      bool   `json:"status" db:"status"`
+	Active      bool   `json:"active" db:"active"`
 }
