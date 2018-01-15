89bf02d55b02847559688a9ecaf500f43776a469
Kian Ostad <c@kianostad.com>
2018-01-15 18:15:39 +0330

    campaign list - chart
    fix #4537

--- cmd/codegen/main.go
+++ cmd/codegen/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/clickyab/services/codegen/datatable"    // Datatable
 	_ "github.com/clickyab/services/codegen/enums"        // Enums plugin
 	_ "github.com/clickyab/services/codegen/gin"          // Gin plugin
+	_ "github.com/clickyab/services/codegen/graph"        // Graph
 	_ "github.com/clickyab/services/codegen/models_mysql" // Models plugin
 	"github.com/clickyab/services/codegen/plugins"
 	_ "github.com/clickyab/services/codegen/restful"  // Datatable
