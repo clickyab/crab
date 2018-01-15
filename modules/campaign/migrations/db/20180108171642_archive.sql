4e9bad3958325184f7ed5a73bb39b3481c50f87a
dara <dara51php@gmail.com>
2018-01-15 15:46:28 +0330

    add migration

--- modules/campaign/migrations/db/20180108171642_archive.sql
+++ modules/campaign/migrations/db/20180108171642_archive.sql
@@ -5,5 +5,5 @@ ALTER TABLE campaigns ADD archive_at TIMESTAMP DEFAULT NULL  NULL;
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE campaigns FROP archive_at;
+ALTER TABLE campaigns DROP archive_at;
 
