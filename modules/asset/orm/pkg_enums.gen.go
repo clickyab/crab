c2cfc2aea48a6b36bfbcd9b6eb426b3f2195fbc1
Kian Ostad <c@kianostad.com>
2018-01-21 19:05:00 +0330

    codegen

--- modules/asset/orm/pkg_enums.gen.go
+++ modules/asset/orm/pkg_enums.gen.go
@@ -6,7 +6,7 @@ import (
 	"database/sql/driver"
 
 	"github.com/clickyab/services/array"
-	"github.com/clickyab/services/trans"
+	"github.com/clickyab/services/gettext/t9e"
 )
 
 // IsValid try to validate enum value on ths type
@@ -30,10 +30,10 @@ func (e *ISPKind) Scan(src interface{}) error {
 	case nil:
 		b = make([]byte, 0)
 	default:
-		return trans.E("unsupported type")
+		return t9e.G("unsupported type")
 	}
 	if !ISPKind(b).IsValid() {
-		return trans.E("invaid value")
+		return t9e.G("invalid value")
 	}
 	*e = ISPKind(b)
 	return nil
@@ -42,7 +42,7 @@ func (e *ISPKind) Scan(src interface{}) error {
 // Value try to get the string slice representation in database
 func (e ISPKind) Value() (driver.Value, error) {
 	if !e.IsValid() {
-		return nil, trans.E("invalid status")
+		return nil, t9e.G("invalid status")
 	}
 	return string(e), nil
 }
