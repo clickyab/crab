e1c1e7b7e241d97db791533014ff168bf65de1e9
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2018-04-15 16:48:39 +0430

    add financial tables structure fixes #511

---
+++ modules/financial/migrations/db/20180414210204_manual_cash_changes.sql
@@ -0,0 +1,22 @@
+
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+DROP TABLE IF EXISTS `manual_cash_changes`;
+
+CREATE TABLE `manual_cash_changes` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `domain_id` INT(10) UNSIGNED NOT NULL,
+  `user_id` INT(10) UNSIGNED NOT NULL,
+  `operator_id` INT(10) UNSIGNED NOT NULL,
+  `reason` ENUM('gift', 'manual_pay', 'refound') NOT NULL,
+  `amount` INT(10) NOT NULL,
+  `description` TEXT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS `manual_cash_changes`;
+
