56234bd1ac2d9b37867d9c4999c25fd2bff33839
dara <dara51php@gmail.com>
2018-04-23 10:51:19 +0430

    add conv miration
    Fix #5153

---
+++ modules/dataworker/migrations/db/20180423055535_conv.sql
@@ -0,0 +1,112 @@
+
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+DROP TABLE IF EXISTS `impressions`;
+CREATE TABLE impressions
+(
+  id VARCHAR(63) NOT NULL,
+  campaign_id INT NOT NULL,
+  creative_id INT NOT NULL,
+  cpc INT NOT NULL,
+  cpm INT NOT NULL,
+  delay INT NOT NULL,
+  share INT NOT NULL,
+  diff INT DEFAULT NULL ,
+  publisher_name VARCHAR(63) NOT NULL,
+  publisher_type ENUM("app", "web") NOT NULL,
+  supplier VARCHAR(63) NOT NULL,
+  seat_id VARCHAR(63) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT impressions_shadow_created_at_id_pk PRIMARY KEY (created_at, id)
+);
+
+DROP TABLE IF EXISTS `impression_details`;
+
+CREATE TABLE impression_details
+(
+  id VARCHAR(63) NOT NULL,
+  ref VARCHAR(127),
+  page VARCHAR(127),
+  ip VARCHAR(31) NOT NULL,
+  ua VARCHAR(255) NOT NULL,
+  user_id VARCHAR(63) NOT NULL,
+  lat DOUBLE,
+  lon DOUBLE,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT impression_detail_created_at_imp_id_pk PRIMARY KEY (created_at, id)
+);
+
+DROP TABLE IF EXISTS `clicks`;
+
+CREATE TABLE clicks
+(
+  id VARCHAR(63) NOT NULL,
+  campaign_id INT NOT NULL,
+  creative_id INT NOT NULL,
+  cpc INT NOT NULL,
+  share INT NOT NULL,
+  delay INT NOT NULL,
+  publisher_name VARCHAR(63) NOT NULL,
+  publisher_type ENUM("app", "web") NOT NULL,
+  supplier VARCHAR(63) NOT NULL,
+  seat_id VARCHAR(63) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT click_shadow_created_at_id_pk PRIMARY KEY (created_at, id)
+);
+
+DROP TABLE IF EXISTS `click_details`;
+
+
+CREATE TABLE click_details
+(
+  id VARCHAR(63) NOT NULL,
+  ref VARCHAR(127),
+  page VARCHAR(127),
+  ip VARCHAR(31) NOT NULL,
+  ua VARCHAR(255) NOT NULL,
+  user_id VARCHAR(63) NOT NULL,
+  lat DOUBLE,
+  lon DOUBLE,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT click_detail_created_at_imp_id_pk PRIMARY KEY (created_at, id)
+);
+
+CREATE TABLE conversions
+(
+  id VARCHAR(63) NOT NULL,
+  campaign_id INT NOT NULL,
+  creative_id INT NOT NULL,
+  cpc INT NOT NULL,
+  share INT NOT NULL,
+  delay INT NOT NULL,
+  publisher_name VARCHAR(63) NOT NULL,
+  publisher_type ENUM("app", "web") NOT NULL,
+  supplier VARCHAR(63) NOT NULL,
+  seat_id VARCHAR(63) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT conversions_created_at_id_pk PRIMARY KEY (created_at, id)
+);
+
+CREATE TABLE conversion_details
+(
+  id VARCHAR(63) NOT NULL,
+  ref VARCHAR(127),
+  page VARCHAR(127),
+  ip VARCHAR(31) NOT NULL,
+  ua VARCHAR(255) NOT NULL,
+  user_id VARCHAR(63) NOT NULL,
+  lat DOUBLE,
+  lon DOUBLE,
+  created_at TIMESTAMP NOT NULL,
+  CONSTRAINT click_detail_created_at_imp_id_pk PRIMARY KEY (created_at, id)
+);
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE conversion_details;
+DROP TABLE conversions;
+DROP TABLE click_details;
+DROP TABLE clicks;
+DROP TABLE impression_details;
+DROP TABLE impressions;
+
