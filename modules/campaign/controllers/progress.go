ad70bd57e9e058a1823246d68c2323668ab3ba84
slifer2015 <dara51php@gmail.com>
2018-05-01 15:11:25 +0430

    progress bar in campaign
    Fix #4539

---
+++ modules/campaign/controllers/progress.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"clickyab.com/crab/modules/campaign/errors"
+	"clickyab.com/crab/modules/campaign/orm"
+	"clickyab.com/crab/modules/domain/middleware/domain"
+	"clickyab.com/crab/modules/user/aaa"
+	userErrors "clickyab.com/crab/modules/user/errors"
+	"clickyab.com/crab/modules/user/middleware/authz"
+	"github.com/rs/xmux"
+)
+
+// getCampaignProgress getCampaignProgress
+// @Rest {
+// 		url = /progress/:id
+//		protected = true
+// 		method = GET
+//		resource = get_campaign:self
+// }
+func (c Controller) getCampaignProgress(ctx context.Context, r *http.Request) (*orm.CampaignProgress, error) {
+	idInt, err := strconv.ParseInt(xmux.Param(ctx, "id"), 10, 0)
+	if err != nil {
+		return nil, errors.InvalidIDErr
+	}
+	currentUser := authz.MustGetUser(ctx)
+	dm := domain.MustGetDomain(ctx)
+	cpManger := orm.NewOrmManager()
+	campaign, err := orm.NewOrmManager().FindCampaignByIDDomain(idInt, dm.ID)
+	if err != nil {
+		return nil, errors.NotFoundError(idInt)
+	}
+
+	owner, err := aaa.NewAaaManager().FindUserWithParentsByID(campaign.UserID, dm.ID)
+	if err != nil {
+		return nil, userErrors.NotFoundWithDomainError(dm.Name)
+	}
+
+	// check access
+	_, ok := aaa.CheckPermOn(owner, currentUser, "get_campaign", dm.ID)
+	if !ok {
+		return nil, errors.AccessDenied
+	}
+	res := cpManger.GetProgressData(campaign.ID, dm.ID)
+	return &res, nil
+
+}
--- modules/campaign/orm/campaign_funcs.go
+++ modules/campaign/orm/campaign_funcs.go
@@ -199,3 +199,42 @@ func (m *Manager) FillCampaignGraph(
 
 	return res
 }
+
+// CampaignProgress campaign progress object
+type CampaignProgress struct {
+	TotalSpend  int64        `json:"total_spend" db:"total_spend"`
+	Click       int64        `json:"click" db:"click"`
+	Imp         int64        `json:"imp" db:"imp"`
+	DailyBudget int64        `json:"daily_budget" db:"daily_budget"`
+	TotalBudget int64        `json:"total_budget" db:"total_budget"`
+	Ctr         float64      `json:"ctr" db:"ctr"`
+	AvgCPC      float64      `json:"avg_cpc" db:"avg_cpc"`
+	MaxBid      int64        `json:"max_bid" db:"max_bid"`
+	OwnerEmail  string       `json:"owner_email" db:"owner_email"`
+	Status      Status       `json:"status" db:"status"`
+	Kind        CampaignKind `json:"kind" db:"kind"`
+}
+
+// GetProgressData get progress bar data
+func (m *Manager) GetProgressData(campaignID, domainID int64) CampaignProgress {
+	var res CampaignProgress
+	q := fmt.Sprintf(`SELECT
+							c.total_spend AS total_spend,
+							COALESCE(SUM(cd.click),0) AS click,
+							COALESCE(SUM(cd.imp),0) AS imp,
+							c.daily_budget AS daily_budget,
+							c.total_budget AS total_budget,
+							COALESCE((SUM(cd.click)/SUM(cd.imp))*10,0) AS ctr,
+							COALESCE(SUM(cd.cpc)/SUM(cd.click),0) AS avg_cpc,
+							c.max_bid AS max_bid,
+							owner.email AS owner_email,
+							c.status AS status,
+							c.kind AS kind
+							FROM %s AS c
+							INNER JOIN %s AS owner ON owner.id=c.user_id
+							LEFT JOIN %s AS cd ON cd.campaign_id=c.id WHERE c.id=? AND c.domain_id=? GROUP BY c.id LIMIT 1
+							`, CampaignTableFull, aaa.UserTableFull, CampaignDetailTableFull)
+
+	assert.Nil(m.GetRDbMap().SelectOne(&res, q, campaignID, domainID))
+	return res
+}
