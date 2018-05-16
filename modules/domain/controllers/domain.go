893213c974b676ca2e545bfa1dbb2262cfd4f523
mohsen mirzakhani <mohsenmirzakhany@gmail.com>
2018-05-16 05:28:27 +0000

    add route to create new domain
    Fix #5226

--- modules/domain/controllers/domain.go
+++ modules/domain/controllers/domain.go
@@ -2,7 +2,11 @@ package controllers
 
 import "github.com/clickyab/services/framework/controller"
 
-// Controller Controller
+// Controller is the controller for the ad package
+// @Route {
+//		group = /domain
+//		middleware = domain.Access
+// }
 type Controller struct {
 	controller.Base
 }
