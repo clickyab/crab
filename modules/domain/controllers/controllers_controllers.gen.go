c079049ec1aa7c1fa5b83825942d62787aa930ca
mohsen mirzakhani <mohsenmirzakhany@gmail.com>
2018-05-19 13:34:12 +0000

    codegen

--- modules/domain/controllers/controllers_controllers.gen.go
+++ modules/domain/controllers/controllers_controllers.gen.go
@@ -15,8 +15,8 @@ import (
 //		method = post
 //		payload = createDomainPayload
 //		middleware = authz.Authenticate
-//		resource = create_new_domain:global
-//		200 = createDomainResult
+//		resource = god:global
+//		200 = orm.Domain
 //		400 = controller.ErrorResponseSimple
 //		401 = controller.ErrorResponseSimple
 //		403 = controller.ErrorResponseSimple
@@ -24,6 +24,28 @@ import (
 func (c *Controller) createDomainPost(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	pl := c.MustGetPayload(ctx).(*createDomainPayload)
 	res, err := c.createDomain(ctx, r, pl)
+	if err != nil {
+		framework.Write(w, err, http.StatusBadRequest)
+		return
+	}
+	framework.Write(w, res, http.StatusOK)
+}
+
+// editDomain to domain
+// @Route {
+// 		url = /edit/:id
+//		method = put
+//		payload = editDomainPayload
+//		middleware = authz.Authenticate
+//		resource = god:global
+//		200 = orm.Domain
+//		400 = controller.ErrorResponseSimple
+//		401 = controller.ErrorResponseSimple
+//		403 = controller.ErrorResponseSimple
+// }
+func (c *Controller) editDomainPut(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	pl := c.MustGetPayload(ctx).(*editDomainPayload)
+	res, err := c.editDomain(ctx, r, pl)
 	if err != nil {
 		framework.Write(w, err, http.StatusBadRequest)
 		return
