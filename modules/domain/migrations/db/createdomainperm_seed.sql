8e96bc2aa959009e72804024987b947ee4d9276f
mohsen mirzakhani <mohsenmirzakhany@gmail.com>
2018-05-19 13:33:03 +0000

    create domain fixed to use god perm

--- modules/domain/migrations/db/createdomainperm_seed.sql
+++
@@ -1,9 +0,0 @@
-
--- +migrate Up
--- SQL in section 'Up' is executed when this migration is applied
-INSERT INTO role_permission (role_id, perm, scope) VALUES (3,"create_new_domain","global");
-INSERT INTO role_permission (role_id, perm, scope) VALUES (4,"create_new_domain","global");
-
--- +migrate Down
--- SQL section 'Down' is executed when this migration is rolled back
-DELETE FROM role_permission WHERE perm ="create_new_domain" AND role_id IN (3,4);
