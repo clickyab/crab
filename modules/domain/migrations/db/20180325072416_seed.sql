3164d2dac817a17e332ce55b0db16996cb834715
slifer2015 <dara51php@gmail.com>
2018-05-22 16:01:33 +0430

    fix migration

--- modules/domain/migrations/db/20180325072416_seed.sql
+++ modules/domain/migrations/db/20180325072416_seed.sql
@@ -1,8 +1,8 @@
 
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-INSERT INTO domains (id,name,description) VALUES (1,'staging.crab.clickyab.ae','clickyab staging');
-INSERT INTO domains (id,name,description) VALUES (2,'127.0.0.1','local stuff');
+INSERT INTO domains (id,domain_base,description) VALUES (1,'staging.crab.clickyab.ae','clickyab staging');
+INSERT INTO domains (id,domain_base,description) VALUES (2,'127.0.0.1','local stuff');
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
