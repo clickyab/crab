71fc7451d9c65777b874c21f461d3f3d3ea437f0
mohsen <mohsenmirzakhany@gmail.com>
2018-05-27 10:20:49 +0430

    fix domain owner roles

--- modules/domain/migrations/db/20180526072954_domain_owner_role.sql
+++ modules/domain/migrations/db/20180526072954_domain_owner_role.sql
@@ -1,9 +1,9 @@
 
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-INSERT INTO roles (id,name, description, domain_id) VALUES (9,'Owner','Domain Owner role',1);
-INSERT INTO roles (id,name, description, domain_id) VALUES (10,'Owner','Domain Owner role',2);
+INSERT INTO roles (id,name, description, domain_id) VALUES (14,'Owner','Domain Owner role',1);
+INSERT INTO roles (id,name, description, domain_id) VALUES (15,'Owner','Domain Owner role',2);
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-DELETE FROM roles WHERE name="Owner" AND id IN (9,10);
+DELETE FROM roles WHERE name="Owner" AND id IN (14,15);
