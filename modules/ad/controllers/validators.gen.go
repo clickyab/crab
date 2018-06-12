afd4e8c907ce3aa85980b468f1e6e97f1bdf9d9b
slifer2015 <dara51php@gmail.com>
2018-06-12 19:01:18 +0430

    gen stuff

--- modules/ad/controllers/validators.gen.go
+++ modules/ad/controllers/validators.gen.go
@@ -13,6 +13,82 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
+func (pl *BannerAssetPayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	err := func(in interface{}) error {
+		if v, ok := in.(interface {
+			ValidateExtra(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+		}); ok {
+			return v.ValidateExtra(ctx, w, r)
+		}
+		return nil
+	}(pl)
+	if err != nil {
+		return err
+	}
+	errs := validator.New().Struct(pl)
+	if errs == nil {
+		return nil
+	}
+	res := middleware.GroupError{}
+	for _, i := range errs.(validator.ValidationErrors) {
+		switch i.Field() {
+		case "CTA":
+			res["cta"] = trans.E("invalid value")
+
+		case "VideoImage":
+			res["video_image"] = trans.E("invalid value")
+
+		case "Video":
+			res["video"] = trans.E("invalid value")
+
+		case "BannerImage":
+			res["banner_image"] = trans.E("invalid value")
+
+		default:
+			logrus.Panicf("the field %s is not translated", i)
+		}
+	}
+	if len(res) > 0 {
+		return res
+	}
+	return nil
+}
+
+func (pl *CreateBannerCreative) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	err := func(in interface{}) error {
+		if v, ok := in.(interface {
+			ValidateExtra(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+		}); ok {
+			return v.ValidateExtra(ctx, w, r)
+		}
+		return nil
+	}(pl)
+	if err != nil {
+		return err
+	}
+	errs := validator.New().Struct(pl)
+	if errs == nil {
+		return nil
+	}
+	res := middleware.GroupError{}
+	for _, i := range errs.(validator.ValidationErrors) {
+		switch i.Field() {
+		case "Name":
+			res["name"] = trans.E("invalid value")
+
+		case "URL":
+			res["url"] = trans.E("invalid value")
+
+		default:
+			logrus.Panicf("the field %s is not translated", i)
+		}
+	}
+	if len(res) > 0 {
+		return res
+	}
+	return nil
+}
+
 func (pl *NativeAssetPayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	err := func(in interface{}) error {
 		if v, ok := in.(interface {
@@ -123,6 +199,44 @@ func (p *changeStatusPayload) Validate(ctx context.Context, w http.ResponseWrite
 	res := middleware.GroupError{}
 	for _, i := range errs.(validator.ValidationErrors) {
 		switch i.Field() {
+		default:
+			logrus.Panicf("the field %s is not translated", i)
+		}
+	}
+	if len(res) > 0 {
+		return res
+	}
+	return nil
+}
+
+func (p *createBannerPayload) Validate(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	err := func(in interface{}) error {
+		if v, ok := in.(interface {
+			ValidateExtra(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+		}); ok {
+			return v.ValidateExtra(ctx, w, r)
+		}
+		return nil
+	}(p)
+	if err != nil {
+		return err
+	}
+	errs := validator.New().Struct(p)
+	if errs == nil {
+		return nil
+	}
+	res := middleware.GroupError{}
+	for _, i := range errs.(validator.ValidationErrors) {
+		switch i.Field() {
+		case "CampaignID":
+			res["campaign_id"] = trans.E("invalid value")
+
+		case "Creative":
+			res["creative"] = trans.E("invalid value")
+
+		case "Type":
+			res["banner_type"] = trans.E("invalid value")
+
 		default:
 			logrus.Panicf("the field %s is not translated", i)
 		}
