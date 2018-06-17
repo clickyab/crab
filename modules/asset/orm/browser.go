555b662ea02a79ab9bc642be735db354705ae06e
slifer2015 <dara51php@gmail.com>
2018-06-17 23:57:41 +0430

    os and browser migration changes
    Fix #5585

--- modules/asset/orm/browser.go
+++ modules/asset/orm/browser.go
@@ -25,15 +25,20 @@ const (
 // Browser model in database
 // @Model {
 //		table = browsers
-//		primary = false, name
-//		find_by = name
+//		primary = true, id
+//		find_by = name,id
 //		list = yes
 // }
 type Browser struct {
+	ID        int64          `json:"id" db:"id"`
 	CreatedAt time.Time      `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at" db:"updated_at"`
 	DeletedAt mysql.NullTime `json:"deleted_at" db:"deleted_at"`
 	Name      string         `json:"name" db:"name"`
+	Mobile    bool           `json:"mobile" db:"mobile"`
+	Desktop   bool           `json:"desktop" db:"desktop"`
+	Tablet    bool           `json:"tablet" db:"tablet"`
+	Other     bool           `json:"other" db:"other"`
 }
 
 // ListActiveBrowsers find active browsers by name
